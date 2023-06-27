0ba74ac090aece9b2a5859d2c7563fd00f2c9152
JungHaeSung <gotjd0230@naver.com>
2023-06-27 21:05:38 +0900

    함지앱 navigation 미디어 쿼리 작업

--- src/apps/HamjiApp/HamjiApp.js
+++ src/apps/HamjiApp/HamjiApp.js
@@ -1,13 +1,24 @@
 import React, { useState, useRef, useEffect } from 'react';
 import './HamjiApp.css'
 
+// 한기대 생활관 앱(함지관 이름을 따서 함지앱)
+// 
+
 function HamjiApp() {
 
   return (
     <div className='container'>
       <div className='hamji-navigation'>
-        <div className='hamji-navigation-logo'></div>
-        <div className='hamji-navigation-buttons'></div>
+        <div className='hamji-navigation-buttons'>
+          <div className='hamji-navigation-buttons-logo hamji-navigation-button'>Hamji</div>
+          <div className='hamji-navigation-buttons-main'>
+            <div className='hamji-navigation-buttons-main-notification hamji-navigation-button'>공지사항</div>
+            <div className='hamji-navigation-buttons-main-bulletin hamji-navigation-button'>게시판</div>
+            <div className='hamji-navigation-buttons-main-cat hamji-navigation-button'>고양이</div>
+            <div className='hamji-navigation-buttons-main-information hamji-navigation-button'>정보</div>
+          </div>
+          <div className='hamji-navigation-buttons-more hamji-navigation-button'>더보기</div>
+        </div>
       </div>
       <div className='hamji-main'>  
         <div className='hamji-main-content'>
