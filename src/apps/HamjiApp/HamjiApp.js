7af7af8722fcf25ec173283882f58903b7d909ec
JungHaeSung <gotjd0230@naver.com>
2023-07-05 16:37:24 +0900

    중간에 그냥

--- src/apps/HamjiApp/HamjiApp.js
+++ src/apps/HamjiApp/HamjiApp.js
@@ -1,31 +1,101 @@
 import React, { useState, useRef, useEffect } from 'react';
 import './HamjiApp.css'
-import Logo from './components/NavigationButtons/Logo'
-import Notification from './components/NavigationButtons/Notification'
-import Bulletin from './components/NavigationButtons/Bulletin'
-import Cat from './components/NavigationButtons/Cat'
-import Information from './components/NavigationButtons/Information'
-import More from './components/NavigationButtons/More'
+import styled from 'styled-components';
 
 // 한기대 생활관 앱(함지관 이름을 따서 함지앱)
-// 
 
+// navigation styled components
+const NVButton = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: row;`
+const NVContainer = styled.div`
+  width: 220px;
+  padding: 12px 12px 20px 12px;
+  position: relative;
+
+  @media (max-width: 1280px) {
+    width: 48px;
+  }
+  @media (max-width: 720px) {
+    display: none;
+  }`
+const NVButtonBox = styled.div`
+  width: 100%;
+  height: 100%;
+  position: relative;
+  & > .NV-logo {
+    width: 100%;
+    height: 90px;
+    display: flex;
+    flex-direction: row;
+    align-items: center;  
+  }
+  & > .NV-more {
+    height: 35px;
+    position: absolute;
+    bottom: 6px; 
+    @media (max-height: 372px) {
+      position: static;
+    }
+  }`
+const NVMainButtonBox = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  & > * {
+    width: 100%;
+    height: 35px; 
+    padding-top: 5px;
+    padding-bottom: 5px;
+    margin-bottom: 7px;
+  }`
+const NVIconImg = styled.img`
+  width: 30px;
+  height: 30px;`
+const NVButtonWord = styled.p`
+  @media (max-width: 1280px) {
+    display: none;
+  }`
+
+// navigation components
+function NVLinkButton({children, src, className}) {
+  return(
+    <NVButton className={className}>
+      <NVIconImg src={src} />
+      <NVButtonWord>{children}</NVButtonWord>
+    </NVButton>
+  )
+}
+function Navigation() {
+  return (
+    <NVContainer>
+      <NVButtonBox>
+        <NVLinkButton className={'NV-logo'} src={'./images/bulletin.png'}>Hamji</NVLinkButton>
+        <NVMainButtonBox>
+          <NVLinkButton src={'./images/bulletin.png'}>공지사항</NVLinkButton>
+          <NVLinkButton src={'./images/bulletin.png'}>게시판</NVLinkButton>
+          <NVLinkButton src={'./images/bulletin.png'}>고양이</NVLinkButton>
+          <NVLinkButton src={'./images/bulletin.png'}>정보</NVLinkButton>
+        </NVMainButtonBox>
+        <NVLinkButton className={'NV-more'} src={'./images/bulletin.png'}>더보기</NVLinkButton>
+      </NVButtonBox>
+    </NVContainer>
+  )
+}
+
+// main styled components
+const MainContainer = styled.div``
+const MCContainer = styled.div``
+const MSContainer = styled.div``
+// main components
+
+// 앱
 function HamjiApp() {
 
   return (
     <div className='container'>
-      <div className='hamji-navigation'>
-        <div className='hamji-navigation-buttons'>
-          <Logo />
-          <div className='hamji-navigation-buttons-main'>
-            <Notification />
-            <Bulletin />
-            <Cat />
-            <Information />
-          </div>
-          <More />
-        </div>
-      </div>
+      <Navigation />
       <div className='hamji-main'>  
         <div className='hamji-main-content'>
           <div className='hamji-main-content-header'></div>
--- src/apps/Test/Test.js
+++ src/apps/Test/Test.js
@@ -1,6 +1,7 @@
-import React from "react";
+import React, { useState, useRef, useEffect } from "react";
+import { styled } from "styled-components";
 
-function Test() {
+function Testt() {
   return (
     <div>
     something test
@@ -8,4 +9,88 @@ function Test() {
   )
 }
 
+const Button = styled.button`
+  width: 20px;
+  height: 20px;
+  margin-left: 10px;
+  color: red;
+  background-color: white;
+  border: 1px solid red;
+  `
+function Test() {
+  const [list, setList] = useState([
+    {id: 0, checked: false, content: '체크리스트'}, 
+    {id: 1, checked: true, content: 'index, check, content'},
+  ])
+  const tempList = list
+  const divRef = useRef();
+  function handleCheckbox(e) {
+    const listIndex = e.target.value;
+    tempList[listIndex].checked = !tempList[listIndex].checked
+    setList(tempList);
+  }
+  function handleRemove(e) {
+    e.preventDefault();
+    const target = e.target.parentNode;
+    for (let i = 0; i < target.parentNode.childNodes.length; i++) {
+      if(target.parentNode.childNodes[i] === target) {
+        tempList.splice(i, 1)
+        target.parentNode.removeChild(target);
+        break;
+      }
+    }
+    arrangeId();
+  }
+  function arrangeId() {
+    divRef.current.childNodes.forEach((node, index)=>{
+      tempList[index].id = index;
+      divRef.current.childNodes[index].key = index;
+    });
+  }
+
+  const inputRef = useRef();
+  function handleAdd(e) {
+    e.preventDefault();
+    const contentToAdd = inputRef.current.value;
+    tempList.push({id: tempList.length, checked: false, content: contentToAdd});
+    setList(tempList);
+    divRef.current.append(
+    
+    
+    <div key={tempList.length}>
+      <input type="checkbox" value={tempList.length} onClick={(e)=>handleCheckbox(e)}/>
+      {contentToAdd}
+      <Button type="button" onClick={(e)=>handleRemove(e)}>x</Button>
+    </div>
+    
+    
+    )
+    console.log(tempList);
+    console.log(list);
+  }
+  useEffect(() => {
+    divRef.current.childNodes.forEach((node, index)=>{
+      node.childNodes[0].checked = list[index].checked;
+    })
+  }, []);
+  return (
+    <div>
+      <div ref={divRef} style={{margin: '10px'}}>
+        {list.map((element, index) => {
+          return (
+            <div key={index}>
+              <input type="checkbox" value={index} onClick={(e)=>handleCheckbox(e)}/>
+              {element.content}
+              <Button type="button" onClick={(e)=>handleRemove(e)}>x</Button>
+            </div>
+          )
+        })}
+      </div>
+      <form>
+        <input ref={inputRef} style={{width: '230px'}}/>
+        <button type="submit" onClick={(e)=>handleAdd(e)}>add</button>
+      </form>
+    </div>
+  )
+}
 export default Test;
